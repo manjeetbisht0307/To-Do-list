e2d4f421181cc09e001bd76058abdc2df7744235
Manjeet SiBi <manjeetbisht0307@gmail.com>
2025-12-02 17:47:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Input values:
 # 1. Add Task
-# 2. Edit Task
-# 3. Delete Task
+# 2. Show Tasks
+# 3. Edit Task
 # 4. Exit
 # Select an option: 1
 # Enter task: Buy groceries
@@ -10,17 +10,21 @@
 task={}
 list=True
 while list:
-    user_input=int(input(("1. Add \n2. Show Tasks \n3. Delete Task \n4. Exit\n")))
+    user_input=int(input(("1. Add \n2. Show Tasks \n3. Edit Task as done \n4. Exit\nOnly Numeric\n")))
     if user_input==1:
-        add_task=input(("Add your task in To-Do list"))
+        add_task=input(("Add your task in To-Do list\n"))
         task[add_task]="undone"
+        print("Task added successfully!")
     elif user_input==2:
         print(f"Tasks-\n")
         for item in task:
-            print(item)
+            print(f"{item}-{task[item]}")
+        print("\n")
     elif user_input==3:
-        edit_task=input("Enter the Task").lower()
+        edit_task=input("Enter the Task to mark as done-\n").lower()
         task[edit_task]="Done"
+    else:
+        list=False
 
 
 
